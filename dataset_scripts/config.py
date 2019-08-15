c84b2413955d93d97b6d6862ca96296fde76497d
tataganesh <tataganesh95@gmail.com>
2019-08-15 23:10:12 +0530

    REFAC: Added config file
    - Some file paths places in config file
    - Simplified process of generating csv used for tf records conversion

---
+++ dataset_scripts/config.py
@@ -0,0 +1,4 @@
+TRAIN_FOLDER = 'images/train'
+TEST_FOLDER = 'images/test'
+IMAGES_FOLDER_PATH = '/home/tata/Projects/hand_detector/dataset/_LABELLED_SAMPLES'
+DATASET_METADATA_PATH = '/home/tata/Projects/hand_detector/dataset/metadata.mat'
