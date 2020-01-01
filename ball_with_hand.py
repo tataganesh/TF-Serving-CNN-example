20d8ef02958fc6dd1ee1f3462f22684c02272a22
tataganesh <tataganesh95@gmail.com>
2020-01-01 16:59:17 +0530

    REFAC: Functions refactor, files removal
    - Improved naming of some variables
    - Improved line / word spacing
    - Moved functions for logical separation
    - Deleted some unused files

--- ball_with_hand.py
+++ ball_with_hand.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import os
-import IPython
 import helpers
 import hand_detection_client
 from tf_serving import serving_config
@@ -12,26 +11,12 @@ parser.add_argument("--hold", help="Wait for key press for every image", action=
 parser.add_argument("--method", help="Grpc / Rest", default="grpc")
 args = parser.parse_args()
 
-def draw_box_with_ball(boxes, ball_box, image):
-    box_colour_mapping = dict()
-    if not ball_box:
-        for box in boxes:
-            box_colour_mapping[box] = (0, 0, 255)
-    else:
-        for box in boxes:
-            if helpers.bb_intersection_over_union(box, ball_box) > 0.5:
-                box_colour_mapping[box] = (0, 255, 0)
-            else:
-                box_colour_mapping[box] = (0, 0, 255)
-    for box, colour in box_colour_mapping.items():
-        cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), colour, 3)
-    return image
-
-print(args.method)
 if args.method == "rest": 
     hands_detect_model = hand_detection_client.handsDetectionRest(serving_config.host, serving_config.restAPIport, serving_config.model_name)
 else:   
     hands_detect_model = hand_detection_client.handsDetection(serving_config.host, serving_config.port, serving_config.model_name)
+
+
 def run_on_video(vid_path=None):
     if vid_path is not None:
         cam = cv2.VideoCapture(vid_path)
@@ -42,9 +27,9 @@ def run_on_video(vid_path=None):
         if not ret_val:
             break
         ball_box = helpers.find_ball(bgrImage)
-        hand_boxes, time_taken = hands_detect_model.predict(bgrImage, confidence=0.1)
+        hand_boxes, time_taken = hands_detect_model.predict(bgrImage)
         if hand_boxes:
-            bgrImage = helpers.draw_box_with_ball(hand_boxes, ball_box,bgrImage)
+            bgrImage = helpers.draw_box_with_ball(hand_boxes, ball_box, bgrImage)
         cv2.putText(bgrImage,'%.3f'%(time_taken), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
         cv2.imshow('my webcam', bgrImage)
         if args.hold:
--- hand_detection_client.py
+++ hand_detection_client.py
@@ -7,10 +7,22 @@ import tensorflow as tf
 from tf_serving import serving_config
 import requests
 import json
-import IPython
 import time
+CONFIDENCE = 0.1
+RESIZE_WIDTH = 300
+RESIZE_HEIGHT = 300
 
-
+def extractBboxes(networkOutput, imageHeight, imageWidth):
+    detection_boxes, detection_scores, detection_classes = map(
+    np.squeeze, networkOutput)
+    hand_boxes = list()
+    for box, score in zip(detection_boxes, detection_scores):
+        if(score > CONFIDENCE):
+            y1, x1, y2, x2 = box[0]*imageHeight, box[1]*imageWidth, box[2]*imageHeight, box[3]*imageWidth
+            hand_box = map(int, [x1, y1, x2, y2])
+            hand_boxes.append(tuple(hand_box))
+    return hand_boxes
+    
 class handsDetection:
     def __init__(self, host, port, model_name, signature_name="serving_default"):
         self.channel = grpc.insecure_channel(host + ":" + str(port))
@@ -19,10 +31,12 @@ class handsDetection:
         self.request.model_spec.name = model_name
         self.request.model_spec.signature_name = signature_name
         self.count = 0
-        self.inputPtime = 0
+        self.inputProcessingTime = 0
         self.reqResTime = 0
-    def predict(self, image, confidence):
-        rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)  
+
+    def predict(self, image):
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        height, width = rgb_image.shape[:2]
         self.count += 1 
         # if self.count % 5 == 0:
             # self.request.model_spec.version_label = "canary"
@@ -32,7 +46,8 @@ class handsDetection:
         start = time.time()
         # Input preparation     
         self.request.inputs['inputs'].CopyFrom(tf.contrib.util.make_tensor_proto(image_np_expanded, shape=image_np_expanded.shape))
-        self.inputPtime = time.time() - start
+        self.inputProcessingTime = time.time() - start
+        # Prediction
         result = self.stub.Predict(self.request, 10.0)
         end = time.time()
         self.reqResTime += end - start
@@ -41,14 +56,7 @@ class handsDetection:
         detection_classes = tf.make_ndarray(outputs["detection_classes"])
         detection_boxes = tf.make_ndarray(outputs["detection_boxes"])
         detection_scores = tf.make_ndarray(outputs["detection_scores"])
-        detection_boxes, detection_scores, detection_classes = map(
-            np.squeeze, [detection_boxes, detection_scores, detection_classes])
-        hand_boxes = list()
-        for box, score in zip(detection_boxes, detection_scores):
-            if(score > confidence):
-                y1,x1,y2,x2 = box[0]*image.shape[0], box[1]*image.shape[1], box[2]*image.shape[0], box[3]*image.shape[1]
-                x1, y1, x2, y2 = map(int, [x1,y1,x2,y2])
-                hand_boxes.append((int(x1), int(y1), int(x2), int(y2)))
+        hand_boxes = extractBboxes([detection_boxes, detection_scores, detection_classes], height, width)
         return hand_boxes, time_taken
 
 class handsDetectionRest:
@@ -57,35 +65,31 @@ class handsDetectionRest:
         self.signature_name = signature_name
         self.headers = {"content-type": "application/json"}
         self.count = 0
-        self.inputPtime = 0
+        self.inputProcessingTime = 0
         self.reqResTime = 0
-    def predict(self, image, confidence, version_number=1):
+
+    def predict(self, image, version_number=1):
         self.count += 1
         rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        rgb_image = cv2.resize(rgb_image, (300, 300))
+        height, width = rgb_image.shape[:2]
+        rgb_image = cv2.resize(rgb_image, (RESIZE_WIDTH, RESIZE_HEIGHT))
         image_np_expanded = np.expand_dims(rgb_image, axis=0) # Sending a batch of 1
         start = time.time()
         # Input preparation 
         data = json.dumps({"signature_name": self.signature_name, "inputs": {"inputs": image_np_expanded.tolist()}})
-        self.inputPtime = time.time() - start
+        self.inputProcessingTime = time.time() - start
         url = self.urlPrefix + str(version_number) + ":predict"
-        json_response = requests.post(url, data=data, headers=self.headers)
+        # predict
+        response = requests.post(url, data=data, headers=self.headers)
         end = time.time()
         self.reqResTime += (end - start) 
         time_taken = end - start
-        result = json.loads(json_response.text)
+        result = json.loads(response.text)
         output = result['outputs']
         detection_classes = output["detection_classes"]
         detection_boxes = output["detection_boxes"]
         detection_scores = output["detection_scores"]
-        detection_boxes, detection_scores, detection_classes = map(
-            np.squeeze, [detection_boxes, detection_scores, detection_classes])
-        hand_boxes = list()
-        for box, score in zip(detection_boxes, detection_scores):
-            if(score > confidence):
-                y1,x1,y2,x2 = box[0]*image.shape[0], box[1]*image.shape[1], box[2]*image.shape[0], box[3]*image.shape[1]
-                x1, y1, x2, y2 = map(int, [x1,y1,x2,y2])
-                hand_boxes.append((int(x1), int(y1), int(x2), int(y2)))
+        hand_boxes = extractBboxes([detection_boxes, detection_scores, detection_classes], height, width)
         return hand_boxes, time_taken
 
 
